5a369519db879a75b51337881f9f772481e2c0f2
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2023-09-03 11:50:33 +0200

    first commit

---
+++ src/main/java/com/feignsongifyclient/FeignSongifyClientApplication.java
@@ -0,0 +1,13 @@
+package com.feignsongifyclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignSongifyClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeignSongifyClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/feignsongifyclient/FeignSongifyClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feignsongifyclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeignSongifyClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
